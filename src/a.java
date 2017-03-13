3e2370cb3a406b7e764cbc7e27d7d10b6f5444e6
Никитин Михаил Дмитриевич <nikitinmd.18@sch239.net>
2017-03-13 11:39:40 +0300

    1 commit

---
+++ src/a.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/**
+ * Created by nikitinmd.18 on 13.03.2017.
+ */
+public class a {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a+" "+a*a+" "+a*a*a+" "+a*a*a*a);
+    }
+}
---
+++ src/b.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/**
+ * Created by nikitinmd.18 on 13.03.2017.
+ */
+public class b {
+    public static void main(String[] args) {
+        int num, arg;
+        for(num = 2; num 0; arg++) {
+        }
+        if(arg == num) System.out.println(arg + "\t");
+    }
+}
